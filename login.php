9d258b4061fb2f939b358af261ee2a8f062d5a54
Hodaya <hodayanir43@gmail.com>
2020-09-13 19:41:56 +0300

    updated login.php

--- login.php
+++ login.php
@@ -4,16 +4,23 @@ include "mysqlConfig.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $u_email = $_POST["email"];
     $u_pass = $_POST["password"];
+    $resObj = new stdClass();
 
-    $sql_query="SELECT s1.email, s1.pass, s2.email, s2.pass FROM students AS s1 JOIN teachers AS s2 WHERE (s1.email='$u_email' AND s1.pass='$u_pass') OR (s2.email='$u_email' AND s2.pass='$u_pass')";
+    $sql_query="SELECT email, first_name FROM students WHERE (email='$u_email' AND pass='$u_pass')  
+                UNION
+                SELECT email, first_name  FROM teachers WHERE (email='$u_email' AND pass='$u_pass')";
     $res=mysqli_query($link, $sql_query);
     if (mysqli_num_rows($res) > 0) {
-        setcookie('first_name', $row[3]);
-        header("Location: index.html");
+        $row = mysqli_fetch_row($res);
+        setcookie('first_name', $row[1]);
+        setcookie('email', $u_email);
+        $resObj->response = "Success";
     }
     else {
-        header("Location: register_form.php");
+        $resObj->response = "Fail";
     }
+    $myJSON = json_encode($resObj);
+    echo $myJSON;
 
 }
-?>
+
